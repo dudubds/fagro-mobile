0d3d57b935ffd81af00811791c56514073fd9b2e
dudubds <dudubds26@gmail.com>
2025-06-11 22:16:12 -0300

    teste bd

--- src/app/cadastro/agricultor.tsx
+++ src/app/cadastro/agricultor.tsx
@@ -1,13 +1,70 @@
 import { View, Text, StyleSheet, TextInput, Pressable, Alert } from "react-native";
-import { useState } from "react";
-import { Link, router } from "expo-router";
+import { useState, useCallback } from "react";
+import { Link, router, useFocusEffect } from "expo-router";
 import { supabase } from "../../utils/supabase";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AgricultorCadastro() {
+
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [confirmPassword, setConfirmPassword] = useState('');
+
+async function handleSignUp() {
+  if (password !== confirmPassword) {
+    Alert.alert("Erro", "As senhas não coincidem!");
+    return;
+  }
+
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+  });
+
+  if (error) {
+    Alert.alert("Erro no Cadastro", error.message);
+  } else if (data.user) {
+    Alert.alert(
+      "Cadastro realizado!",
+      "Enviamos um link de confirmação para o seu email. Por favor, verifique sua caixa de entrada (e spam) para ativar sua conta."
+    );
+    router.push('/home');
+  }
+}
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Cadastro Agricultor</Text>
-    </View>
+
+      <TextInput
+      style={styles.input}
+      placeholder="seuemail@gmail.com"
+      value={email}
+      onChangeText={setEmail}
+      keyboardType="email-address"
+      autoCapitalize="none"/>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Crie uma senha"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        autoCapitalize="none"/>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Confirme sua senha"
+        value={confirmPassword}
+        onChangeText={setConfirmPassword}
+        secureTextEntry={true}
+        autoCapitalize="none"/>
+
+      <Pressable style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </Pressable>
+
+    </SafeAreaView>
   )
 }
 
@@ -24,4 +81,27 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
+  input: {
+    width: '80%',
+    height: 45,
+    backgroundColor: '#FFFFE4',
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    borderRadius: 25,
+    borderColor: '#299640',
+    borderWidth: 1.5,
+  },
+  button: {
+    width: '80%',
+    height: 45,
+    backgroundColor: '#299640',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 500,
+  },
 });
+
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { useState } from "react";
 import { Link } from "expo-router";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Home() {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -18,61 +19,58 @@ export default function Home() {
   };
 
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("../../assets/logo-normal.png")}
-        style={styles.logo}
-      />
-      <Text style={styles.titulo}>Bem-vindo (a)</Text>
-
-      <View>
-        <Pressable onPress={() => Alert.alert("pressionou")}>
-          <Text style={styles.button}>Entrar</Text>
-        </Pressable>
-
-        <Pressable onPress={() => toggleModal()}>
-          <Text style={styles.button}>Cadastrar</Text>
-        </Pressable>
-      </View>
-
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={isModalVisible}
-        onRequestClose={toggleModal}
-      >
-        <TouchableOpacity
-          style={styles.modal}
-          activeOpacity={1}
-          onPressOut={() => setIsModalVisible(false)}
-        >
-          <Pressable>
-              <View style={styles.modalContent}>
-                <View style={styles.imageRow}>
-                  <Link href={"/cadastro/agricultor"} asChild>
-                    <Pressable style={styles.modalSubContent}>
-                      <Image
-                        source={require("../../assets/agricultores.png")}
-                        style={styles.imagem}
-                      />
-                      <Text style={styles.text}>Agricultor</Text>
-                    </Pressable>
-                  </Link>
-                  <Link href={"/cadastro/consumidor"} asChild>
-                    <Pressable style={styles.modalSubContent}>
-                      <Image
-                        source={require("../../assets/consumidores.png")}
-                        style={styles.imagem}
-                      />
-                      <Text style={styles.text}>Consumidor</Text>
-                    </Pressable>
-                  </Link>
-                </View>
-              </View>
+    <SafeAreaView style={styles.container}>
+        <Image
+          source={require("../../assets/logo-normal.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.titulo}>Bem-vindo (a)</Text>
+        <View>
+          <Pressable onPress={() => Alert.alert("pressionou")}>
+            <Text style={styles.button}>Entrar</Text>
           </Pressable>
-        </TouchableOpacity>
-      </Modal>
-    </View>
+          <Pressable onPress={() => toggleModal()}>
+            <Text style={styles.button}>Cadastrar</Text>
+          </Pressable>
+        </View>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={toggleModal}
+        >
+          <TouchableOpacity
+            style={styles.modal}
+            activeOpacity={1}
+            onPressOut={() => setIsModalVisible(false)}
+          >
+            <Pressable>
+                <View style={styles.modalContent}>
+                  <View style={styles.imageRow}>
+                    <Link href={"/cadastro/agricultor"} asChild>
+                      <Pressable style={styles.modalSubContent}>
+                        <Image
+                          source={require("../../assets/agricultores.png")}
+                          style={styles.imagem}
+                        />
+                        <Text style={styles.text}>Agricultor</Text>
+                      </Pressable>
+                    </Link>
+                    <Link href={"/cadastro/consumidor"} asChild>
+                      <Pressable style={styles.modalSubContent}>
+                        <Image
+                          source={require("../../assets/consumidores.png")}
+                          style={styles.imagem}
+                        />
+                        <Text style={styles.text}>Consumidor</Text>
+                      </Pressable>
+                    </Link>
+                  </View>
+                </View>
+            </Pressable>
+          </TouchableOpacity>
+        </Modal>
+    </SafeAreaView>
   );
 }
 
